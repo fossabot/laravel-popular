a2ac6d0f972eacdcddb3012c1f5df99979f19166
Nur Wachid <wachid@outlook.com>
2020-08-05 06:13:04 +0700

    update service provider

--- src/LaravelPopularServiceProvider.php
+++ src/LaravelPopularServiceProvider.php
@@ -4,7 +4,56 @@
 namespace Turahe\LaravelPopular;
 
 
-class LaravelPopularServiceProvider
-{
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
 
+class LaravelPopularServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @param Filesystem $filesystem
+     * @return void
+     */
+    public function boot(Filesystem $filesystem)
+    {
+        if (function_exists('config_path')) { // function not available and 'publish' not relevant in Lumen
+            $this->publishes([
+                __DIR__.'/config/popular.php' => config_path('popular.php'),
+            ], 'config');
+
+            $this->publishes([
+                __DIR__.'/Database/Migrations/create_visits_table.php.stub' => $this->getMigrationFileName($filesystem),
+            ], 'migrations');
+        }
+
+        $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_permission_tables.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_permission_tables.php")
+            ->first();
+    }
 }
