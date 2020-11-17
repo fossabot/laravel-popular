79a6725cde4725c3bbc2f0c6c0d811d564ccce32
Nur Wachid <wachid@outlook.com>
2020-11-17 15:53:11 +0700

    concept model

--- src/Models/Visit.php
+++ src/Models/Visit.php
@@ -4,6 +4,10 @@ namespace Turahe\LaravelPopular\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Visit
+ * @package Turahe\LaravelPopular\Models
+ */
 class Visit extends Model
 {
     protected $fillable = ['ip', 'date', 'model_id', 'model_type'];
